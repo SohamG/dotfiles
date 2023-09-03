70c5fdc751b057c220851984d35df2d6a55282d8
Soham S Gumaste <sgumas2@uic.edu>
2023-09-03 15:12:09 -0500

    Emacs bump

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,6 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-view-program-list '(("emacs" ("") "emacsclient")))
+ '(TeX-view-program-selection
+	 '(((output-dvi has-no-display-manager)
+			"dvi2tty")
+		 ((output-dvi style-pstricks)
+			"dvips and gv")
+		 (output-dvi "xdvi")
+		 (output-pdf "PDF Tools")
+		 (output-html "xdg-open")))
  '(column-number-mode t)
  '(connection-local-criteria-alist
 	 '(((:machine "sohamg.xyz")
@@ -105,11 +114,11 @@
 			((agenda "" nil)
 			 (alltodo "" nil))
 			nil)
-		 ("x" "Trying something..." agenda ""
+		 ("x" "Custom Agenda" alltodo ""
 			((org-agenda-overriding-header "Priorities")
 			 (org-agenda-sorting-strategy
 				'(scheduled-up deadline-up todo-state-down time-up))
-			 (org-agenda-prefix-format "  %?l%10s%12t%i")
+			 (org-agenda-prefix-format "  %?T%?l%?:t%10s%i")
 			 (org-overriding-columns-format "%25ITEM %TODO")))))
  '(org-agenda-todo-ignore-with-date t)
  '(org-deadline-warning-days 3)
